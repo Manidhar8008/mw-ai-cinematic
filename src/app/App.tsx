1773bc69545b8b1a07533aaadafea6c8b7ff83c1
Manidhar Pati <manidharmanidhar2@gmail.com>
2026-08-21 12:46:37 +0530

    feat: turn cinematic demo into production landing page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,12 @@
 import { CinematicScene } from './components/CinematicScene'
+import { LandingPage } from './LandingPage'
 
 export default function App() {
-  return <CinematicScene />
+  return (
+    <>
+      <a className="skip-link" href="#main-content">Skip cinematic intro</a>
+      <CinematicScene />
+      <LandingPage />
+    </>
+  )
 }
